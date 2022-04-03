6371f752dbb676a182c90aa0ad4b660a11835288
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-03 21:57:36 +0900

    4300ms 마다 scale 값을 넘겨 text의 크기를 일시적을 늘린 뒤 줄인다

--- app.js
+++ app.js
@@ -21,12 +21,18 @@ const glitch = (element) => {
     element.style.setProperty("--afterTop", `${afterTop}%`);
     element.style.setProperty("--beforeBottom", `${beforeBottom}%`);
     element.style.setProperty("--afterBottom", `${afterBottom}%`);
+    element.style.setProperty("--scale", "1");
 
     count++;
 
     if (count % 23 === 0) {
       const bigSkew = Math.random() * 180 - 90;
       element.style.setProperty("--skew", `${bigSkew}deg`);
+    }
+
+    if (count % 43 === 0) {
+      const bigScale = 1 + Math.random() / 2;
+      element.style.setProperty("--scale", `${bigScale}`);
     }
   }, 100);
 };
